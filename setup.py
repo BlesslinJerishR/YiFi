103ea128ac8348deef9f311d514fdd3a69df1891
Mastermindx33 <mastermindx33@gmail.com>
2021-10-31 07:14:01 +0530

    deploy @base

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(  name='YiFi',
         description='Just a .torrent 8K UHD database downloader for YTS Web Application.',
         packages=find_packages(),
         install_requires=['requests', 'argparse', 'tqdm', 'fake-useragent'],
-        entry_points={'console_scripts': 'YiFi = YiFi.main:main'},
+        entry_points={'console_scripts': 'yifi = yifi.main:main'},
         license=open('LICENSE').read(),
-        keywords=['yts', 'yify','Blesslin Jerish R', 'scraper', 'media', 'download', 'downloader', 'torrent']
+        keywords=['yts', 'YiFy','Blesslin Jerish R', 'scraper', 'media', 'download', 'downloader', 'torrent','yifi']
     )
